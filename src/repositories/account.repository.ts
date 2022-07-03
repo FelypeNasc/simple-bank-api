939c601e84db778bf5e5cc603c21a7e39138f61d
Felype Nascimento <felype.nasc@hotmail.com>
2022-07-03 13:43:33 -0300

    feat: account repo findByAccountData method

--- src/repositories/account.repository.ts
+++ src/repositories/account.repository.ts
@@ -1,6 +1,7 @@
 import PostgresDB from '.';
 import { InternalError } from '../errors';
 import AccountModel from '../models/account.model';
+import { DepositDto } from '../models/dtos/deposit.dto';
 
 export class AccountRepository extends PostgresDB {
   public async create(newAccount: AccountModel): Promise<object> {
@@ -41,6 +42,36 @@ export class AccountRepository extends PostgresDB {
                 `;
       const queryResponse = await client.query(query, [newBalance, id]);
       const response = queryResponse.rows[0];
+
+      return response;
+    } catch (e) {
+      throw new InternalError();
+    }
+  }
+
+  public async findByAccountNumber(
+    userId: string,
+    accountData: DepositDto,
+  ): Promise<AccountModel> {
+    try {
+      const client = await this.pool.connect();
+      const query = `
+                SELECT * FROM mybank.accounts 
+                WHERE user_id = $1 
+                AND account_number = $2 
+                AND account_check_digit = $3 
+                AND agency_number = $4 
+                AND agency_check_digit = $5;
+                RETURNING id, account_number, agency_number, agency_check_digit, account_check_digit, balance;
+                `;
+      const queryResponse = await client.query(query, [
+        userId,
+        accountData.accountNumber,
+        accountData.accountCheckDigit,
+        accountData.agencyNumber,
+        accountData.agencyCheckDigit,
+      ]);
+      const response: AccountModel = queryResponse.rows[0];
 
       return response;
     } catch (e) {
