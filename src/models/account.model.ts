6509320fb2d5511367bc7a9359253d0300198f57
Felype Nascimento <felype.nasc@hotmail.com>
2022-07-03 21:32:03 -0300

    refactor: refactoring models

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,10 +1,11 @@
 export default interface AccountModel {
   id: string;
-  user_id?: string;
-  password: string;
-  agency_number: number;
-  agency_check_digit: number;
-  account_number: number;
-  account_check_digit: number;
+  userId: string;
+  password?: string;
+  agencyNumber: number;
+  agencyCheckDigit: number;
+  accountNumber: number;
+  accountCheckDigit: number;
   balance: number;
+  createdAt?: Date;
 }
--- src/models/transaction.model.ts
+++ src/models/transaction.model.ts
@@ -1,10 +1,10 @@
 export default interface TransactionModel {
   id?: string;
-  origin_account_id?: string;
-  destination_account_id: string;
+  originAccountId?: string;
+  destinationAccountId?: string;
   value: number;
   type: string;
   tax: number;
-  total_value: number;
-  created_at?: Date;
+  totalValue: number;
+  createdAt?: Date;
 }
